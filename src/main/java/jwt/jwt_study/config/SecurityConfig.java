fdb3b13879d705bad8482f554ca472a65af57526
sosow0212 <sosow0212@naver.com>
2022-05-01 21:32:46 +0900

    feat: jwt Filter 등록

--- src/main/java/jwt/jwt_study/config/SecurityConfig.java
+++ src/main/java/jwt/jwt_study/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package jwt.jwt_study.config;
 
+import jwt.jwt_study.filter.MyFilter1;
+import jwt.jwt_study.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -15,10 +19,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CorsFilter corsConfig;
 
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class);
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 안한다라는 뜻
                 .and()
